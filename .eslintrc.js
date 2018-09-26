1104989983e52e4288d9a588f3440b324aff2287
tcdw <tcdw2011@gmail.com>
2018-09-26 19:31:54 +0800

    初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "no-console": "off",
-        "indent": [ "error", 4, { "SwitchCase": 1 } ]
+        "indent": ["error", 4, {
+            "SwitchCase": 1
+        }]
     },
     "env": {
         "node": true
