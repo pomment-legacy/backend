fc88dff285cbe6e9a224f31f4c19852336530a54
tcdw <tcdw2011@gmail.com>
2018-09-29 22:18:06 +0800

    ormthread

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "no-console": "off",
+        "no-underscore-dangle": ["error", {
+            "allowAfterThis": true,
+        }],
         "indent": ["error", 4, {
             "SwitchCase": 1
         }]
