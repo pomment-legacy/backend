0637d43ac3484d0045c67bfafa602cd40c0e03eb
tcdw <tcdw2011@gmail.com>
2018-10-14 09:49:30 +0800

    不再禁用 no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
-        "no-console": "off",
         "no-underscore-dangle": ["error", {
             "allowAfterThis": true,
         }],
