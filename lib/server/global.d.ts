a51227408ee1ca1bc6efac75835ccb070a0cf460
tcdw <tcdw2011@gmail.com>
2018-11-07 18:52:41 +0800

    完善头文件

--- lib/server/global.d.ts
+++ lib/server/global.d.ts
@@ -1,140 +1,104 @@
 interface SiteAdmin {
-    /**
-     * 网站管理员的昵称
-     */
     name: string;
-    /**
-     * 网站管理员的电子邮箱地址
-     */
     email: string;
-    /**
-     * 网站管理员的密码
-     */
     password: string;
 }
 
-interface EmailAuth {
-    /**
-     * 电子邮箱帐号的用户名
-     */
-    user: string;
-    /**
-     * 电子邮箱帐号的密码
-     */
-    pass: string;
-}
-
-interface EmailTransport {
-    /**
-     * 电子邮箱帐号的主机
-     */
-    host: string;
-    /**
-     * 电子邮箱帐号的端口
-     */
-    port: number;
-    /**
-     * 电子邮箱帐号是否使用安全连接
-     */
-    secure: string;
-    /**
-     * 电子邮箱帐号的验证信息
-     */
-    auth: EmailAuth;
-}
-
-interface Email {
-    /**
-     * 是否启用电子邮箱功能
-     */
-    enabled: boolean;
-    /**
-     * 电子邮箱的发信方式
-     */
-    transport: EmailTransport;
-    /**
-     * 电子邮箱显示的发件人
-     */
-    sender: string;
-    /**
-     * 回执邮件标题
-     */
-    receiptTitle: string;
-    /**
-     * 回复提醒邮件标题
-     */
-    replyTitle: string;
-}
-
 interface ReCAPTCHA {
-    /**
-     * 是否启用 reCAPTCHA v3
-     */
     enabled: boolean;
-    /**
-     * reCAPTCHA v3 的 Site Key
-     */
     siteKey: string;
-    /**
-     * reCAPTCHA v3 的 Secret Key
-     */
     secretKey: string;
-    /**
-     * 访客提交的最低分数限制
-     */
     minimumScore: number;
 }
 
+interface PostResults {
+    id: number;
+    name: string;
+    email: string;
+    website: string;
+    parent: number;
+    content: string;
+    hidden: boolean;
+    byAdmin: boolean;
+    receiveEmail: boolean;
+    editKey: string;
+    updatedAt: Date;
+    createdAt: Date;
+};
+
+interface PostItem {
+    id: number;
+    name: string;
+    email: string;
+    website: string;
+    content: string;
+    parent: number;
+    hidden?: boolean;
+    byAdmin: boolean;
+    createdAt: Date;
+};
+
+interface ThreadAttribute {
+    title: string;
+};
+
+interface ThreadItem {
+    url: string;
+    attributes: ThreadAttribute;
+};
+
+declare class PommentData {
+    constructor(workingDir: string);
+    static init(workingDir: string): void;
+    getThreadPath(url: string): void;
+    getThreads(): (ThreadItem)[];
+    getThreadAttribute(): ThreadAttribute;
+    addThreadTitle(url: string, title: string): void;
+    getPosts(url: string, outputEmailHash: boolean, includeHidden: boolean): Promise<PostItem[]>;
+    getPost(url: string, id: number, includeHidden: boolean): Promise<PostItem>;
+    addPost(
+        url: string,
+        name: string,
+        email: string,
+        website: string,
+        content: string,
+        parent?: number,
+        receiveEmail: boolean,
+        byAdmin: boolean,
+        hidden?: boolean): Promise<PostResults>;
+    editPost(
+        url: string,
+        id: number,
+        name: string,
+        email: string,
+        website: string,
+        parent: number,
+        content: string,
+        hidden: boolean,
+        byAdmin: boolean,
+        receiveEmail: boolean,
+        editKey: string): Promise<void>;
+    editPostUser(url: any, id: any, content: any, remove?: any): Promise<void>;
+    getThreadLock(url: string): boolean;
+    setThreadLock(url: string, locked: boolean): void;
+    trashThread(url: any): void;
+}
+
 declare namespace $POC {
-    /**
-     * API 服务器所监听的 IP 地址
-     */
     const apiHost: string;
-    /**
-     * API 服务器所监听的端口
-     */
     const apiPort: number;
-    /**
-     * 本站名称，用于电子邮件的标题
-     */
     const siteName: string;
-    /**
-     * 本站 URL
-     */
     const siteURL: string;
-    /**
-     * API 服务器管理员配置
-     */
     const siteAdmin: SiteAdmin;
-    /**
-     * API 服务器电子邮件配置
-     */
-    const email: Email;
-    /**
-     * API 服务器的盐
-     */
     const salt: string;
-    /**
-     * API 服务器 reCAPTCHA v3 配置
-     */
     const reCAPTCHA: ReCAPTCHA;
-    /**
-     * API 服务器的 API 密钥（加盐的）
-     */
     const apiKey: string;
-    /**
-     * API 服务器是否在代理下运行。该设置会影响日志中访客 IP 地址的精确性
-     */
     const underProxy: boolean;
 }
 
 declare namespace $POM {
-    /**
-     * 服务器是否运行在开发模式
-     */
     const isDev: boolean;
-    /**
-     * 服务器的日志输出级别
-     */
     const logLevel: boolean;
 }
+
+declare const $POD: PommentData;
