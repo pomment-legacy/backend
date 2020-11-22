e16b27a1f8c0d218adc28d0db40391b74595110a
tcdw <tcdw2011@gmail.com>
2020-11-23 00:04:53 +0800

    移除未使用的类型

--- src/interface/webhook.ts
+++ src/interface/webhook.ts
@@ -4,8 +4,6 @@ import { IAuth } from '../lib/auth';
 // eslint-disable-next-line no-shadow
 export enum EventName {
     postAdded = 'post_added',
-    postEdited = 'post_edited',
-    postRemoved = 'post_removed',
 }
 
 export interface IWebhookRequest {
