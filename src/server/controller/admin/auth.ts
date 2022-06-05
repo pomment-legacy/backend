4ea2d7ad0d3c1683338a257afa8a5d7c7378045f
tcdw <tcdw2011@gmail.com>
2022-06-05 15:54:30 +0800

    Finish auth

--- src/server/controller/admin/auth.ts
+++ src/server/controller/admin/auth.ts
@@ -1,16 +1,23 @@
 import { ControllerConfig } from '@/types/server';
 import { PommentComputedContext } from '@/server/main';
 import { sign } from '@/server/permission';
-import { AjaxSuccess } from '@/server/utils/wrapper';
+import { AjaxError, AjaxSuccess } from '@/server/utils/wrapper';
+import crypto from 'crypto';
 
 function handler(ctx: PommentComputedContext) {
+    const { password } = ctx.request.body;
+    const computedPassword = crypto.createHash('sha512').update(password ?? '').digest('hex');
+    if (computedPassword !== ctx.$config.siteAdmin.password) {
+        AjaxError(ctx, 400);
+        return;
+    }
     const expiresIn = 60 * 60 * 12;
     const token = sign(ctx.$config.siteAdmin.name, ctx.$config.siteAdmin.secret, expiresIn);
     AjaxSuccess(ctx, { token, expiresIn });
 }
 
 const auth: ControllerConfig = {
-    method: 'get',
+    method: 'post',
     path: '/admin/auth',
     handler,
 };
